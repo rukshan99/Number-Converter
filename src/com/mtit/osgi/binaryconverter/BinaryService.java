61a4d20165a44daaf240742075b10b61f2921dd8
ASUS <sahansulochana27@gmail.com>
2021-04-12 23:31:23 +0530

    IT19147024 Binary Converter

---
+++ src/com/mtit/osgi/binaryconverter/BinaryService.java
@@ -0,0 +1,8 @@
+package com.mtit.osgi.binaryconverter;
+
+public interface BinaryService {
+
+	public String DecimalToBinary(int decimal);
+	public String OctalToBinary(String inputValue);
+	public String HexaToBinary(String Hexa); 
+}
