a69bcbc850667e2d2b592f3ce5ea13c90f586c80
MSI PC <navodinith@gmail.com>
2021-04-13 15:12:02 +0530

    hexa service created

---
+++ src/com/mtit/osgi/hexaconverter/HexaService.java
@@ -0,0 +1,8 @@
+package com.mtit.osgi.hexaconverter;
+
+public interface HexaService {
+	public String DecimalHexadecimal(int decimal);
+	public String OctalHexadecimal(String oct);
+	public String BinaryHexadecimal(String binary);
+
+}
