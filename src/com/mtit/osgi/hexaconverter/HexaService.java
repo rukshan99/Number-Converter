a994929a7ce71119a58dcbeea3cc602bf4d97b4c
MSI PC <navodinith@gmail.com>
2021-04-13 17:32:12 +0530

    hexa decimal converter

---
+++ src/com/mtit/osgi/hexaconverter/HexaService.java
@@ -0,0 +1,9 @@
+  
+package com.mtit.osgi.hexaconverter;
+
+public interface HexaService {
+	public String DecimalHexadecimal(int decimal);
+	public String OctalHexadecimal(String oct);
+	public String BinaryHexadecimal(String binary);
+
+}
